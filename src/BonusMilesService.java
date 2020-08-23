82f47f15c48ce241180645b96da5dccd1efc8c9a
Nikoly <turnik3580@gmail.com>
2020-08-23 12:28:31 +0300

    add project

---
+++ src/BonusMilesService.java
@@ -0,0 +1,25 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+        return price;
+    }
+
+    int miles = price / eachSpentMoney;
+    int price = 100;
+    int eachSpentMoney = 20;
+    int bonus = 1;
+
+        return miles;
+}
+}
+
+//public class Main {
+ //   public static void main(String[] args) {
+ //       int ticketcCost = 100;
+ //       int eachSpentMoney = 20;
+ //       int bonus = 1;
+ //       int mile = ticketcCost / eachSpentMoney * bonus;
+
+  //      System.out.println(mile);
+  //  }
+//}
